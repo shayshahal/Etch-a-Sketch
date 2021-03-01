c346046f59cc8dfe15d6eb767cd5f95aa327d7fa
shayshahal <shay840@gmail.com>
2021-03-01 22:41:56 +0200

    made trail color random and fade to black

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const container = document.getElementById("container");
+function random255()
+{
+    return Math.floor(Math.random() * (256 - 0) + 0);
+}
+
 function makeGrid(rows, cols)
 {
     document.documentElement.style.setProperty('--grid-rows', rows);
@@ -6,7 +11,18 @@ function makeGrid(rows, cols)
     for (let i = 0; i < rows; i++) {   
         for (let j = 0; j < cols; j++) {
             let div = document.createElement('div');  
-            div.addEventListener("mouseover", function(e) {e.target.classList.add("trail")});
+
+            div.addEventListener("mouseover", function addTrail(e)
+            {                
+                if(!e.target.style.backgroundColor)
+                    e.target.style.backgroundColor = `rgb(${random255()}, ${random255()}, ${random255()})`;
+                else
+                {
+                    const regex = /[0-9]{1,3}/g;
+                    let rgb = e.target.style.backgroundColor.match(regex);
+                    e.target.style.backgroundColor = `rgb(${rgb[0] - rgb[0]/10}, ${rgb[1] - rgb[1]/10}, ${rgb[2] - rgb[2]/10})`;
+                }
+            });
             div.textContent = "";
             container.appendChild(div); 
         }
@@ -20,8 +36,8 @@ function clearGrid(e)
     makeGrid(size, size);
 }
 makeGrid(16, 16);
+
 //Code to prevent user from typing a number larger than 100
-const input = document.getElementById("size");
 size.oninput = function () {
     if (this.value > 100) {
         this.value = 100;
